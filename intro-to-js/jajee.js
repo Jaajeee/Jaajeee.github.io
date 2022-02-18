a3152ce4471544613e447cd1c934a8ecb2ff609f
Wichuda Kruejanta <wichuda.kru@gmail.com>
2022-02-18 14:20:06 +0700

    js 2 practice

--- intro-to-js/jajee.js
+++ intro-to-js/jajee.js
@@ -1,15 +1,32 @@
 
 
-
 function askMyName() {
-    const name = prompt('What Is My Name')
+    const name = prompt('What Is Your Name')
  
     if (name != null) {
-       document.getElementById("j.name").innerHTML = "My name is " + name;
+       document.getElementById("j.name").innerHTML = "My name is " + name ;
     }
  }
  
  function helloWorld() {
- const h1message = document.getElementById("j.name").innerHTML 
-    alert (h1message + 'แสงแดดที่ร้อนจ้า ยังแพ้ตัวข้าที่ร้อนเงิน')
+   const h1message = document.getElementById("j.name").innerHTML 
+    alert (h1message  + " แสงแดดที่ร้อนจ้า ยังแพ้ตัวข้าที่ร้อนเงิน ")
  }
+
+
+ function calAge() {
+    const Age = prompt ("Year of yourbirthday is")
+    alert ("'ยินดีด้วย นายอยู่ประเทศนี้มา" + ((2022) - Age) + 'ปีแล้ว');
+ }
+
+
+function loadProfile(){
+   const name = (" Jajee " + "")
+   const province = (" Lampang " + "")
+   
+   document.getElementById("my-name").innerHTML = name 
+   document.getElementById("my-province").innerHTML = province
+
+
+
+}
